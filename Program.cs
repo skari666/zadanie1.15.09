93a014e83d71fb5abab50e8e6a9616b4e085d919
skari666 <144269531+skari666@users.noreply.github.com>
2023-09-15 13:32:39 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine('Введите координаты точки A:');
+        double x1 = Convert.ToDouble(Console.ReadLine());
+        double y1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine('Введите координаты точки B:');
+        double x2 = Convert.ToDouble(Console.ReadLine());
+        double y2 = Convert.ToDouble(Console.ReadLine());
+
+        double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+        Console.WriteLine($'Длина отрезка AB = {distance:F3}');
+    }
+}
